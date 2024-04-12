9f03e005c1b0f8b223a8315c5e0d728eef8d55d0
basant <3bpro2000@gmail.com>
2024-04-12 19:28:13 +0200

    MainServer added and is made connected in sync to firebase

---
+++ Main Server/main_server_app/Emergency.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainServer
+{
+    internal class Emergency
+    {
+        private string ambulance;
+        private string finger_print;
+        private string location;
+        public string Ambulance 
+        { get  => ambulance ; set => ambulance = value; }
+        public string FingerPrint {  get=> finger_print; set => finger_print = value; }
+        public string Location { get=> location; set => location = value; }
+
+        public override string ToString() => $"Ambulance ID: {ambulance}\nFinger Print: {finger_print}\nLocation: {location}";
+        
+    }
+}
