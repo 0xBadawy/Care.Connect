ab82998f0ea4161184ff4ff905c6b10b6f0557c6
Moahemd-basawy <mohamedbadawysayed02@gmail.com>
2024-05-19 17:27:01 +0300

    add Assits

--- Hospital Data update/hospital_data_update_App/obj/Debug/net6.0-windows/hospital_data_update_App.AssemblyInfo.cs
+++ Hospital Data update/hospital_data_update_App/obj/Debug/net6.0-windows/hospital_data_update_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hospital_data_update_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcd6b5000be0d54ec5f4b348cab105468dc17153")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("hospital_data_update_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hospital_data_update_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
