4eeae77f61bb57027d7c842b62a349aae58d61e0
basant <3bpro2000@gmail.com>
2024-07-11 03:06:12 +0300

    Reception_App was added

---
+++ Reception_App/Login.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Reception_App
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input_HospitalID = new System.Windows.Forms.TextBox();
+            this.Input_Password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Input_HospitalID
+            // 
+            this.Input_HospitalID.Location = new System.Drawing.Point(530, 83);
+            this.Input_HospitalID.Name = "Input_HospitalID";
+            this.Input_HospitalID.Size = new System.Drawing.Size(100, 22);
+            this.Input_HospitalID.TabIndex = 0;
+            // 
+            // Input_Password
+            // 
+            this.Input_Password.Location = new System.Drawing.Point(530, 188);
+            this.Input_Password.Name = "Input_Password";
+            this.Input_Password.Size = new System.Drawing.Size(100, 22);
+            this.Input_Password.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(441, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Hospital ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(441, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Location = new System.Drawing.Point(514, 271);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(75, 23);
+            this.btn_Login.TabIndex = 4;
+            this.btn_Login.Text = "Login";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Input_Password);
+            this.Controls.Add(this.Input_HospitalID);
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input_HospitalID;
+        private System.Windows.Forms.TextBox Input_Password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Login;
+    }
+}
+
